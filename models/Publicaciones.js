9ebc8dae9dcd6555fa7ef5b8a993906f2a5a0fa5
sergio <66255714+serg274@users.noreply.github.com>
2023-09-27 04:23:18 +0200

    modificaciones

--- models/Publicaciones.js
+++ models/Publicaciones.js
@@ -1,35 +1,39 @@
-const { DataTypes, Sequelize, sequelize  } = require('../database');
+const { DataTypes, Sequelize, sequelize } = require("../database");
 
-const Publicacion = sequelize.define('Publicacion', {
+const Publicacion = sequelize.define(
+  "Publicacion",
+  {
     id: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
     },
     titulo: {
-        type: DataTypes.STRING(255),
-        allowNull: false
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     detalle: {
-        type: DataTypes.STRING(255),
-        allowNull: false
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     url_imagen: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
+      type: DataTypes.STRING(255),
+      allowNull: true,
     },
     fecha_publicacion: {
-        type: DataTypes.DATE,
-        allowNull: true,
-        defaultValue: sequelize.literal('CURRENT_TIMESTAMP')
+      type: DataTypes.DATE,
+      allowNull: true,
+      defaultValue: sequelize.literal("CURRENT_TIMESTAMP"),
     },
     firma_autor: {
-        type: DataTypes.STRING(255),
-        allowNull: false
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
-},{
+  },
+  {
     timestamps: false,
-    tableName: 'publicaciones'
-});
+    tableName: "publicaciones",
+  }
+);
 
 module.exports = Publicacion;
